d50d7bc6f21a9f281eee8166e119007afb8b0cfc
Marc-Antoine Hinse <marc_antoine_hinse@hotmail.com>
2016-03-12 22:12:41 -0500

    Gradle file yo

--- android/app/src/main/java/com/mirego/rebelchat/controllers/UsersSingleton.java
+++ android/app/src/main/java/com/mirego/rebelchat/controllers/UsersSingleton.java
@@ -1,7 +1,43 @@
 package com.mirego.rebelchat.controllers;
 
+import android.content.Context;
+
+import com.mirego.rebelchat.models.User;
+
+import java.util.ArrayList;
+
 /**
  * Created by marc-antoinehinse on 2016-03-12.
  */
-public class UsersSingleton {
+public class UsersSingleton implements UsersController.UsersCallback {
+    private static UsersSingleton instance;
+    private static ArrayList<User> users;
+    private static UsersController usersController;
+
+    private UsersSingleton(){
+        users = new ArrayList<User>();
+        usersController = new UsersControllerImpl();
+    };
+
+    public static UsersSingleton getInstance() {
+        if(instance == null) {
+            instance = new UsersSingleton();
+        }
+
+        return instance;
+    }
+
+    public void init(final Context context) {
+        usersController.getUsers(context, null, this);
+    }
+
+    @Override
+    public void onUsersSuccess(ArrayList<User> users) {
+        this.users
+    }
+
+    @Override
+    public void onUsersFailed() {
+
+    }
 }
